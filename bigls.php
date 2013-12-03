dd4114e8b7f9165f57f537d54e5428f6d7333afe
Harry Sufehmi <sufehmi@gmail.com>
2013-12-03 19:49:41 +0700

    typo / non-essential changes

--- bigls.php
+++ bigls.php
@@ -6,16 +6,14 @@
 
         version		: 1.0.2
 
-	description	: 
-
-        License		: GPL v2
+        license		: GPL v2
 			  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 			  http://vlsm.org/etc/gpl-unofficial.id.html
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License v2 (links provided above) for more details.
+			This program is distributed in the hope that it will be useful,
+			but WITHOUT ANY WARRANTY; without even the implied warranty of
+			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+			GNU General Public License v2 (links provided above) for more details.
 ----------------------------------------------------------------*/
 
 // ------------------- VARIABLES --------------------------------
@@ -107,6 +105,7 @@ foreach (array_keys($opts) as $opt) switch ($opt) {
 		exit(0);
 
 }; // foreach (array_keys($opts) as $opt) switch ($opt) {
+
 
 
 // start going through list of files ===============================================
